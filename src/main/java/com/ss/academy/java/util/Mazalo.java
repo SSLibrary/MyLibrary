a144e0bcb4e0bf18125d34a3cc874090528ec6d2
mbeleva88 <m_d_b@mail.bg>
2015-11-25 23:23:38 +0200

    Test: renaming class Mazalo.

--- src/main/java/com/ss/academy/java/util/Mazalo.java
+++ src/main/java/com/ss/academy/java/util/Mazalo.java
@@ -1,10 +1,10 @@
 package com.ss.academy.java.util;
 
-import java.text.DecimalFormat;
-import java.util.List;
 
-import com.ss.academy.java.model.rating.Rating;
 
-public class RatingCalculator {
+public abstract class Mazalo {
+	
+	public void Test()
+	{}
 	
 }
