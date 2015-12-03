12abda442d8d4591118299579e64fd39c0d9e4fe
IvanVidenov <videnov.ivan@gmail.com>
2015-12-03 04:37:00 -0800

    table history

--- library.sql
+++ library.sql
@@ -133,3 +133,4 @@ CONSTRAINT fk_book_id FOREIGN KEY (book_id) REFERENCES author_books(book_id) ON 
 CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 )ENGINE = InnoDB CHARSET=utf8;
 
+
