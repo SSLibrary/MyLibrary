744541d4196e767fccb4f881dee96fadba557686
IvanVidenov <videnov.ivan@gmail.com>
2015-12-03 05:15:01 -0800

    history model

---
+++ src/main/java/com/ss/academy/java/model/history/BookHistory.java
@@ -0,0 +1,76 @@
+package com.ss.academy.java.model.history;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "history")
+public class BookHistory {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "user_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userId;
+	
+	@Column(name = "book_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long bookId;
+	
+	@Column(name = "get_date")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Date getDate;
+	
+	@Column(name = "return_date")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Date returnDate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public Long getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(Long bookId) {
+		this.bookId = bookId;
+	}
+
+	public Date getGetDate() {
+		return getDate;
+	}
+
+	public void setGetDate(Date getDate) {
+		this.getDate = getDate;
+	}
+
+	public Date getReturnDate() {
+		return returnDate;
+	}
+
+	public void setReturnDate(Date returnDate) {
+		this.returnDate = returnDate;
+	}
+}
