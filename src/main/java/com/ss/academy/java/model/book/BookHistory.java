5d6dbd223aeac98893534c6d57922d09bbe0fff5
IvanVidenov <videnov.ivan@gmail.com>
2015-12-03 08:36:59 -0800

    changed db relations

--- src/main/java/com/ss/academy/java/model/book/BookHistory.java
+++ src/main/java/com/ss/academy/java/model/book/BookHistory.java
@@ -7,7 +7,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 @Table(name = "history")
@@ -18,10 +22,14 @@ public class BookHistory {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
+	@OneToMany(mappedBy = "userId")
 	@Column(name = "user_id")
+	@JsonManagedReference(value = "userId-book")
 	private int userId;
 	
+	@ManyToOne()
 	@Column(name = "book_id")
+	@JsonManagedReference(value = "book-userId")
 	private int bookId;
 	
 	@Column(name = "get_date")
