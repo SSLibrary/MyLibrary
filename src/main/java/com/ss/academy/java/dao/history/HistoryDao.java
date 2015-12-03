a200b346d7bc9c465474a6facd57dc4f7223afbe
IvanVidenov <videnov.ivan@gmail.com>
2015-12-03 12:28:20 -0800

    HistoryDao interface

--- src/main/java/com/ss/academy/java/dao/history/HistoryDao.java
+++ src/main/java/com/ss/academy/java/dao/history/HistoryDao.java
@@ -3,9 +3,14 @@ package com.ss.academy.java.dao.history;
 import java.util.List;
 
 import com.ss.academy.java.model.book.Book;
+import com.ss.academy.java.model.user.User;
 
 public interface HistoryDao {
 
 	List<Book> showMyHistory();
+
+	void getBook(User userId, Book bookId);
+	
+	void returnBook(Book bookId);
 	
 }
