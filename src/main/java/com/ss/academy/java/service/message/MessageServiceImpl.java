b7375a0bb46e48ff28b7d1b0cf9889657b4db1c8
mbeleva88 <m_d_b@mail.bg>
2015-12-04 17:27:00 +0200

    Added comments in message classes

--- src/main/java/com/ss/academy/java/service/message/MessageServiceImpl.java
+++ src/main/java/com/ss/academy/java/service/message/MessageServiceImpl.java
@@ -17,19 +17,33 @@ public class MessageServiceImpl implements MessageService {
 	@Autowired
 	private MessageDao dao;
 
+	/*
+	 * Find message by message_id
+	 */
 	public Message findById(Integer message_id) {
 		return dao.findById(message_id);
 	}
 
+	/*
+	 * Save message
+	 */
 	public void saveMessage(Message message) {
 		dao.saveMessage(message);
 
 	}
 
+
+	/*
+	 * Find all messages
+	 */
 	public List<Message> findAllMessages() {
 		return dao.findAllMessages();
 	}
 
+
+	/*
+	 * Update the message status from unread to read
+	 */
 	public void updateMessageStatus(Message message) {
 
 		Message entity = dao.findById(message.getMessage_id());
@@ -39,6 +53,9 @@ public class MessageServiceImpl implements MessageService {
 		}
 	}
 
+	/*
+	 * Find all sent messages of the authenticated user
+	 */
 	public List<Message> listAllSentMessages(Integer offset, Integer maxResults, String username) {
 		return dao.listAllSentMessages(offset, maxResults, username);
 	}
@@ -47,6 +64,9 @@ public class MessageServiceImpl implements MessageService {
 		return dao.countSentMessages(username);
 	}
 
+	/*
+	 * Find all received messages of the authenticated user
+	 */
 	public List<Message> listAllReceivedMessages(Integer offset, Integer maxResults, String username) {
 		return dao.listAllReceivedMessages(offset, maxResults, username);
 	}
