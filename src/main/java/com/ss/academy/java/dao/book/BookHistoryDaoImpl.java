c202760925959099b80d58a418d926a5cc269270
VasilSokolov <vasilsokolov0@gmail.com>
2015-12-29 22:14:18 +0200

    fix order of returned book by getDate descending order

--- src/main/java/com/ss/academy/java/dao/book/BookHistoryDaoImpl.java
+++ src/main/java/com/ss/academy/java/dao/book/BookHistoryDaoImpl.java
@@ -43,7 +43,7 @@ public class BookHistoryDaoImpl extends AbstractDao<Long, BookHistory> implement
 	public List<BookHistory> findAllBooksHistory(Integer offset, Integer maxResults) {
 		return getSession()
 				.createCriteria(BookHistory.class)
-//				.addOrder( Order.desc("date"))
+				.addOrder( Order.desc("getDate"))
 				.setFirstResult(offset!=null?offset:0)
 				.setMaxResults(maxResults!=null?maxResults:5)
 				.list();		
@@ -62,8 +62,8 @@ public class BookHistoryDaoImpl extends AbstractDao<Long, BookHistory> implement
 	public List<BookHistory> findAllBooksHistory(Integer offset, Integer maxResults, Integer isReturned) {
 		return getSession()
 				.createCriteria(BookHistory.class)
+				.addOrder( Order.desc("getDate"))
 				.add(Restrictions.eq("isReturned", isReturned))
-//				.addOrder( Order.desc("date"))
 				.setFirstResult(offset!=null?offset:0)
 				.setMaxResults(maxResults!=null?maxResults:5)
 				.list();		
